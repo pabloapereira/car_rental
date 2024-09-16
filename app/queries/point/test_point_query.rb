e768ad85349acfb365e7ed613b211061624be824
pabloapereira <pabloandrei0906@gmail.com>
2024-09-16 15:29:18 -0300

    add dynamic user name

--- app/queries/point/test_point_query.rb
+++ app/queries/point/test_point_query.rb
@@ -3,13 +3,11 @@ module PointQuery
   attr_reader :query
 
   def get_employer
+
     @query = User.select("name").where(name: "Jimmy DeLocke")
 
     @query = @query.map do |entry|
-      {
-        name: entry.name
-      }.values
-      end
+      entry.name
+    end
   end
 end
-
